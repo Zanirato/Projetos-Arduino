c04b6c62819fa28af427da38974c5628e65834fb
Julia Zanirato <juliazaniratoo@gmail.com>
2024-08-05 15:22:50 -0300

    Comentários do programa

--- piscar-led/piscar-led.ino
+++ piscar-led/piscar-led.ino
@@ -1,13 +1,16 @@
+//c++
+//programa para piscar uma led
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode (9, OUTPUT);
+  pinMode (9, OUTPUT); //declaração da porta como saída
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(9, HIGH);
-  delay(1000);
-  digitalWrite(9, LOW);
-  delay(1000);
+  digitalWrite(9, HIGH); //  manda valor alto para a porta 9
+  delay(1000); // aguarda 1 segundo
+  digitalWrite(9, LOW); //  manda valor baixo para a porta 9
+  delay(1000); //  aguarda 1 segundo
 
 }
